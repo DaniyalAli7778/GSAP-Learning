e55f6bd948312fef86b71842e07640c02f1062b2
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-12-03 15:46:37 +0500

    "create cursour and curve svg animation"

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-//  let path =`M 10 80 Q 580 80 1160 80`;
+ let path =`M 10 80 Q 580 80 1160 80`;
 
-//  let finalPath=`M 10 80 Q 580 100 1160 80`;
+ let finalPath=`M 10 80 Q 580 100 1160 80`;
  
 
-//  let String=document.querySelector('#string');
+ let String=document.querySelector('#string');
 
 
-//  String.addEventListener("mousemove",function(dets){
-//   path =`M 10 80 Q  ${dets.x} ${dets.y} 1160 80`
+ String.addEventListener("mousemove",function(dets){
+  path =`M 10 80 Q  ${dets.x} ${dets.y} 1160 80`
 
-//   gsap.to("svg path",{
-//     attr:{d:path},
-//         duration:0.2,
-//     ease:"power3.out"
-//   })
-//         console.log(path);
-//  }  )
+  gsap.to("svg path",{
+    attr:{d:path},
+        duration:0.2,
+    ease:"power3.out"
+  })
+        console.log(path);
+ }  )
 
 
-//  String.addEventListener("mouseleave",function(dets){
-//   path =finalPath;
+ String.addEventListener("mouseleave",function(dets){
+  path =finalPath;
 
-//   gsap.to("svg path",{
-//     attr:{d:finalPath},
-// duration:1.5,
-// ease:"elastic.out(1.2,0.2)"
-//   })
-//         console.log(path);
-//  }  )
+  gsap.to("svg path",{
+    attr:{d:finalPath},
+duration:1.5,
+ease:"elastic.out(1.2,0.2)"
+  })
+        console.log(path);
+ }  )
  
+ // SVG animation end
+
+ // CURSOUR animation starts heres
+
+ const main2 =document.querySelector("#main2");
+
+ const cursour =document.querySelector(".cursour");
+
+ main2.addEventListener("mousemove",function(e){
+     gsap.to(cursour,{
+      x:e.x,
+      y:e.y,
+      ease:"back.out"
+     })
+  
+ })
+  gsap.to("#textAnimation h1",{
+    transform:"translateX(-200%)",
+    scrollTrigger:{
+      trigger:"#textAnimation ",
+      scroller:"body",
+      markers:true,
+      start:"top 0%",
+      end:"top -150%",
+      scrub:2,
+       pin:true,
+       
+    }
+
+  })

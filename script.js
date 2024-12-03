8bef27e0b5060ff9b7e67b474a762c003df2fda7
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-12-03 12:32:11 +0500

    "GSAP-FIRST-PROJECT"

---
+++ script.js
@@ -0,0 +1,31 @@
+//  let path =`M 10 80 Q 580 80 1160 80`;
+
+//  let finalPath=`M 10 80 Q 580 100 1160 80`;
+ 
+
+//  let String=document.querySelector('#string');
+
+
+//  String.addEventListener("mousemove",function(dets){
+//   path =`M 10 80 Q  ${dets.x} ${dets.y} 1160 80`
+
+//   gsap.to("svg path",{
+//     attr:{d:path},
+//         duration:0.2,
+//     ease:"power3.out"
+//   })
+//         console.log(path);
+//  }  )
+
+
+//  String.addEventListener("mouseleave",function(dets){
+//   path =finalPath;
+
+//   gsap.to("svg path",{
+//     attr:{d:finalPath},
+// duration:1.5,
+// ease:"elastic.out(1.2,0.2)"
+//   })
+//         console.log(path);
+//  }  )
+ 

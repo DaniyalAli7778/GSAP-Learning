8e03397b5867ba0178bbf921da7b8f94155a312d
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-12-05 14:18:42 +0500

    "update"

--- script.js
+++ script.js
@@ -1,134 +1,189 @@
- let path =`M 10 80 Q 580 80 1200 80`;
+let path = `M 10 80 Q 580 80 1280 80`;
 
- let finalPath=`M 10 80 Q 580 100 1200 80`;
- 
+let finalPath = `M 10 80 Q 580 100 1280 80`;
 
- let String=document.querySelector('#string');
- 
- String.addEventListener("mousemove",function(dets){
-  path =`M 10 80 Q  ${dets.x} ${dets.y} 1200 80`
 
-  gsap.to("svg path",{
-    attr:{d:path},
-        duration:0.2,
-    ease:"power3.out"
+let String = document.querySelector('#string');
+
+String.addEventListener("mousemove", function (dets) {
+  path = `M 10 80 Q  ${dets.x} ${dets.y} 1280 80`
+
+  gsap.to("svg path", {
+    attr: { d: path },
+    duration: 0.2,
+    ease: "power3.out"
   })
-        console.log(path);
- }  )
+  console.log(path);
+})
 
 
- String.addEventListener("mouseleave",function(dets){
-  path =finalPath;
+String.addEventListener("mouseleave", function (dets) {
+  path = finalPath;
 
-  gsap.to("svg path",{
-    attr:{d:finalPath},
-duration:1.5,
-ease:"elastic.out(1.2,0.2)"
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.5,
+    ease: "elastic.out(1.2,0.2)"
   })
-        console.log(path);
- }  )
- 
- // SVG animation end
+  console.log(path);
+})
 
- // CURSOUR animation starts heres
+// SVG animation end
 
- const main2 =document.querySelector("#main2");
- 
- const cursour =document.querySelector(".cursour");
-const imagediv= document.querySelector('#image');
- main2.addEventListener("mousemove",function(e){
-     gsap.to(cursour,{
-      x:e.x,
-      y:e.y,
-      ease:"back.out"
-     })
-  
- })
+// CURSOUR animation starts heres
 
+const main2 = document.querySelector("#main2");
 
-  gsap.to("#textAnimation1 h1",{
-    transform:"translateX(-200%)",
-    scrollTrigger:{
-      trigger:"#textAnimation1 ",
-      scroller:"body",
-       start:"top 0%",
-      end:"top -150%",
-      scrub:2,
-       pin:true,
-       
-    }
-
+const cursour = document.querySelector(".cursour");
+const imagediv = document.querySelector('#image');
+main2.addEventListener("mousemove", function (e) {
+  gsap.to(cursour, {
+    x: e.x,
+    y: e.y,
+    ease: "back.out"
   })
 
-  gsap.to("#textAnimation2 h1",{
-    transform:"translateX(-200%)",
-    scrollTrigger:{
-      trigger:"#textAnimation2 ",
-      scroller:"body",
-       start:"top 0%",
-      end:"top -150%",
-      scrub:2,
-       pin:true,
-       
-    }
+})
 
+
+gsap.to("#textAnimation1 h1", {
+  transform: "translateX(-200%)",
+  scrollTrigger: {
+    trigger: "#textAnimation1 ",
+    scroller: "body",
+    start: "top 0%",
+    end: "top -150%",
+    scrub: 2,
+    pin: true,
+
+  }
+
+})
+
+gsap.to("#textAnimation2 h1", {
+  transform: "translateX(-200%)",
+  scrollTrigger: {
+    trigger: "#textAnimation2 ",
+    scroller: "body",
+    start: "top 0%",
+    end: "top -150%",
+    scrub: 2,
+    pin: true,
+
+  }
+
+})
+image.addEventListener("mouseenter", function () {
+  cursour.innerHTML = "View More"
+  gsap.to(cursour, {
+    scale: 2,
   })
-  image.addEventListener("mouseenter",function(){
-    cursour.innerHTML="View More"
-    gsap.to(cursour,{
-      scale:2,
+})
+
+
+image.addEventListener("mouseleave", function () {
+  cursour.innerHTML = "",
+    gsap.to(cursour, {
+      scale: 1,
     })
-   })
+})
 
 
-   image.addEventListener("mouseleave",function(){
-    cursour.innerHTML="",
-    gsap.to(cursour,{
-      scale:1,
-    })
-   })
+const menue = document.querySelector('nav i');
+menue.addEventListener('click', function () {
+  const fulldiv = document.querySelector('.full');
+  fulldiv.style.display = "inline-block";
+  var tl1 = gsap.timeline();
 
-
-   const menue =document.querySelector('nav i');
-   menue.addEventListener('click',function(){
-    const fulldiv= document.querySelector('.full');
-fulldiv.style.display="inline-block";
-var tl1 =gsap.timeline();
-
-tl1.to(".full",{
- right:"0%",
-duration:0.77,
- })
-
-
- gsap.from(".full h4",{
-  x:150,
-  duration:0.7,
-  stagger:0.28,
-  opacity:0,
- })
- gsap.from(".full i",{
-  x:-10,
-   opacity:0,
-   duration:0.77,
-   delay:0.28,
-   stagger:1,
+  tl1.to(".full", {
+    right: "0%",
+    duration: 0.77,
   })
 
 
-   })
+  gsap.from(".full h4", {
+    x: 150,
+    duration: 0.7,
+    stagger: 0.28,
+    opacity: 0,
+  })
+  gsap.from(".full i", {
+    x: -10,
+    opacity: 0,
+    duration: 0.77,
+    delay: 0.28,
+    stagger: 1,
+  })
+
+
+})
 
 
 
-   const close =document.querySelector('.full i');
-   close.addEventListener('click',function(){
+const close = document.querySelector('.full i');
+close.addEventListener('click', function () {
 
+
+  const fulldiv = document.querySelector('.full');
+  fulldiv.style.display = "none";
+
+
+
+})
+
+
+
+
+function breakTheText() {
+  const h1 = document.querySelector("#page4 h1")
+  let h1Text = h1.textContent;
+
+  let splittedText = h1Text.split("");
+
+  let halfValue= h1Text.length/2;
    
-    const fulldiv= document.querySelector('.full');
-    fulldiv.style.display="none";
+  var cullter = "";
+  splittedText.forEach(function (elem,idx) {
 
-  
+    if(idx<halfValue){
+    cullter += `<span class="a">${elem}</span>`;
+    }else{
+      cullter += `<span class="b">${elem}</span>`;
 
-   })
+    }
+
+  })
+
+
+
+
+  h1.innerHTML = cullter;
+  console.log(splittedText);
+  console.log(cullter);
+
+}
+
+breakTheText();
+
+
+gsap.from("h1 .a",{
+  y:50,
+  duration:1,
+stagger:0.3,
+delay:0.5,
+  opacity:0,
+
+
+})
+
+gsap.from("h1 .b",{
+  y:50,
+  duration:1,
+stagger:-0.3,
+delay:0.5,
+  opacity:0,
+
+
+})
 
 
